8d03320590c34cbd9e8b9b120d5a4fbe2cf3c41b
Connor Torpy <connor@DESKTOP-CKQ1G24.localdomain>
2023-01-25 17:51:14 -0600

    Added more test cases and commented code

--- hashTable.go
+++ hashTable.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 )
 
+/*
+* hashTable struct
+* points - an array of ints containing receipt point values
+* ids - an array of strings containing reciept ids
+* size - maximum number of elements of the array
+* elements - current number of elements in the array
+*/
 type hashTable struct{
     points []int
     ids    []string
@@ -11,6 +18,12 @@ type hashTable struct{
     elements int
 }
 
+/*
+* Initializes a hashTable of an input size
+*
+* arguments
+* size - the size that the hashTable should be initialized to
+*/
 func Init(size int) *hashTable {
     table := &hashTable{}
     table.size = size
@@ -20,6 +33,16 @@ func Init(size int) *hashTable {
     return table
 }
 
+/*
+* inserts a new key value pair into the hash table
+*
+* arguments
+* table - the hashTable to be inserted into
+* key - the id associated with the request
+* value - the points associated with the receipt
+* tableAddr a pointer to the pointer containing the hashTables location
+* it is changed in the case of a resize
+*/
 func (table *hashTable) insert(key string, value int, tableAddr **hashTable){
 	fmt.Println("insert got: ", value)
 	index := hashFunc(key, table.size)
@@ -47,11 +70,18 @@ func (table *hashTable) insert(key string, value int, tableAddr **hashTable){
     }
 }
 
+/*
+* removes an entry associated with input key from the hashTable
+* note - this function was unused and is therefore untested in the receipt-processor implementation
+*
+* arguments
+* key - id associated with the request
+*/
 func (table *hashTable) remove(key string){
 	index := hashFunc(key, table.size)
 	done := 0
 	for done != 1 {
-	if table.ids[index] == key {  //feels wrong to use == with a string but according to internet this works i guess
+	if table.ids[index] == key {  //feels wrong to use == with a string but according to internet this works
 		done = 1
 		table.ids[index] = ""
 		table.elements -= 1
@@ -64,6 +94,10 @@ func (table *hashTable) remove(key string){
      }
 }
 
+/*
+* returns a new hashTable with 16 times the size as the input hashTable 
+* but containing all the same key value pairs
+*/
 func (table *hashTable) resize() *hashTable{
 	newTable := Init(table.size << 4)
 	i := 0
@@ -76,26 +110,44 @@ func (table *hashTable) resize() *hashTable{
 	return newTable
 }
 
+/*
+* hashFunc takes a key and size and returns an index for the hashTable based on it's
+* function
+* 
+* arguments
+* key - a string that contains the ID of the receipt
+* size - the size of the hashTable, determines the limit of how large a number
+* the hash function can return
+*/
 func hashFunc(key string, size int) int{
 	index := 1
-	i := 0 
+	i := 0
+	// not a great hash function imo expected value is 0 so might tend toward lower numbers
+	// increasing collision chance
 	for i < len(key) {
 //		index += index * 7 + int(key[i])
 		index += index + int(key[i])
 		i += 1
 	}
+	// might be better to absolute value each individual addition
 	if index < 0 {
            index = -index
 	}
+	// modulo size so returned value is within desired range
 	return index % size
 }
-
+/*
+* get returns the point value associated with given key if it exists in the hash table
+* returns -1 on failure to find the receipt of the givne id 
+*
+* arguments
+* key - string of the id associated with the request
+* table - the hashtable to be searched
+*/
 func (table *hashTable) get(key string) int{
 	index := hashFunc(key, table.size)
 	i := 0
 
-	fmt.Println("get first try is index: ", index)
-	fmt.Println("get given ID is: ", key)
 	for i < table.size{
 		if table.ids[index] == key {
 
--- receipt.go
+++ receipt.go
@@ -7,9 +7,17 @@ import (
     "encoding/hex"
     "strings"
     "strconv"
+    "math"
 )
 
-
+/*
+* receipt struct
+* Retailer: string containing the name of the retailer
+* PurchaseDate: String containing the date in the format yyyy-mm-dd
+* PurchaseTime: String containing the time of the purchase in format hh:mm
+* Items: an array of item structs
+* Total: contains the total in the form d+.dd
+*/
 type receipt struct{
 	Retailer     string `json:"retailer"`
 	PurchaseDate string `json:"purchaseDate"`
@@ -17,48 +25,73 @@ type receipt struct{
 	Items        []item `json:"items"`
 	Total        string `json:"total"`
 }
-
+/*
+* item struct
+* ShortDescription: string containing a description of item
+* Price: contains item price in form d+.dd
+*/
 type item struct{
 	ShortDescription string `json:"shortDescription"`
 	Price            string `json:"price"`
 }
 
+/*
+* id struct
+* ID the id given in the form hhhh-hhhh-hhhh-hhhh-hhhh where h means hexadecimal
+*/
 type id struct{
 	ID string `json:"id" uri:"id"`
 }
 
+// hashtable that stores ids and associated points values
 var table *hashTable
 
+/*
+* returns a Json object containing the points value of the requested id
+* path: /receipts/{id}/points
+* 
+* arguments
+* context - a gin context variable that is used to extract the id from the path
+*/
 func getPoints(context *gin.Context){
 	var idIn id
+
 	err := context.BindUri(&idIn)
 	if err != nil {
         context.String(404, "Id format invalid\n")
         }
-	fmt.Println(idIn.ID)
-        points := table.get(idIn.ID)
-	fmt.Println("get got this: ", points)
-        if points == -1 {
+
+	points := table.get(idIn.ID)
+
+	if points == -1 {
         context.String(404, "No receipt found for that id\n")
 	} else {
 	context.JSON(200, gin.H{"points": points})
 	}
 }
 
+/*
+* postReceipt takes a Json receipt object as an argument through the context variable
+* then generates an ID and associated point value and stores it within the hashtable
+*
+* arguments
+* context - a gin context variable that is used to extract the id from the path
+*/
 func postReceipt (context *gin.Context){
     var newReceipt receipt
-    fmt.Println("Posting Receipt")
     
     err := context.BindJSON(&newReceipt)
     if err != nil {
     context.String(400, "The receipt is invalid, failed to bind\n")
     }
     
-    fmt.Println("Receipt Bound")
     ID := getId(&newReceipt)
     points := 0
 
     // get points from retailer's name
+    // note I chose not to upper or lower case or trim this as I believe
+    // it would make it less efficient as those functions would be running these types of compares
+    // anyways
     i := 0
     for i < len(newReceipt.Retailer) {
         if ((int(newReceipt.Retailer[i]) >= 48 && int(newReceipt.Retailer[i]) <= 57) ||
@@ -69,21 +102,21 @@ func postReceipt (context *gin.Context){
 	     i += 1
     }
     
-    fmt.Println("Name Points Received: ", points)
 
     // get points from total
     TotalLen := len(newReceipt.Total)
+
+    // cents should be an integer value associated with the final two digits of newReceipt.Total
     cents, err := strconv.Atoi(newReceipt.Total[TotalLen-2:TotalLen])
-    fmt.Println("cents: ", cents)
     if err != nil {	    
         context.String(400, "The receipt is invalid, total failed to read\n")
     }
+    // I chose not to do cents % 25 for the second part to avoid the division operator
     if(cents == 0) {
         points += 75
     } else if cents == 25 || cents == 50 || cents == 75 {
         points += 25
     }
-    fmt.Println("Total Points Received: ", points)
 
     // get points from items
     i = 0
@@ -93,57 +126,89 @@ func postReceipt (context *gin.Context){
 	    if err != nil {
                 context.String(400, "The receipt is invalid, failed to read an items price\n")
 	    }
-
-            fmt.Println("item with multiple of 3 length found")
-            points += int(price * 0.2) + 1
+            points += int(math.Ceil(price * 0.2))
 	}
         i += 1
     }
     // points for every 2 items
+    // pulled it out of the loop for increased efficiency
     points += (i >> 1) * 5
-    fmt.Println("Item Points Received: ", points)
 
     // points from odd date
+    // note that ascii numbers that are odd have odd ascii values so this does work
     if int(newReceipt.PurchaseDate[9]) & 0x1 != 0 {
         points += 6
     }
-    fmt.Println("Odd Date Points Received: ", points)
 
     //points from time of purchase
     hour, err := strconv.Atoi(newReceipt.PurchaseTime[0:2])
     if err != nil {
         context.String(400, "The receipt is invalid, did not get numbers in first two digits of time\n")    
     }
+    // Wasn't sure If I should include 2:00 technically any time with 2:00 in the purchaseTime
+    // is some number of seconds after 2pm if the prompt means exactly 2pm, not hard to change if this is not
+    // what was intended by the problem
     if hour >= 14 && hour < 16 {
         points += 10
     }
-    fmt.Println("Purchase Time Points Received: ", points)
 
-    //TODO add something to handle identical id attempts in insert method
+    // Right now there is nothing to handle identical ID's being generated which could result in incorrect behavior
+    // currently this is not tested
     table.insert(ID, points, &table) 
-
-    fmt.Println("Returning ID")
     context.JSON(200, gin.H{"id": ID})
 }
 
+/*
+* Generates an id of the form hhhh-hhhh-hhhh-hhhh-hhhh where h is a hexadecimal character
+* based on characteristics of the input receipt
+*
+*/
 func getId(newReceipt *receipt) string{
 
+	// Tried to do something that somewhat emulated the example id's 
+	// (5 sections with hex characters separated by -) but I don't think that this generation
+	// method is ideal since it can have ID collisions which result in undesired behavior
+
+	// this first bit is here because the original method does not function if there aren't at least 4 characters
+	// in the first item's short description and the retailer field
+	RetailerComponent := ""
+	DescriptionComponent := ""
+	if len(newReceipt.Retailer) >= 4 {
+		RetailerComponent = hex.EncodeToString([]byte(newReceipt.Retailer))[0:4]
+	} else {
+		RetailerComponent = hex.EncodeToString([]byte(newReceipt.Retailer))[0:1]
+                RetailerComponent = RetailerComponent + RetailerComponent + RetailerComponent + RetailerComponent
+	}
+	if len(newReceipt.Items[0].ShortDescription) >= 4 {
+		DescriptionComponent = hex.EncodeToString([]byte(newReceipt.Items[0].ShortDescription))[0:4]
+	} else {
+		DescriptionComponent = hex.EncodeToString([]byte(newReceipt.Items[0].ShortDescription))[0:1]
+                DescriptionComponent = DescriptionComponent + DescriptionComponent + DescriptionComponent + 
+		DescriptionComponent
+	}
+
+	// concatenates the 5 sections
 	returnValue := hex.EncodeToString([]byte(newReceipt.Total))[0:4] + "-" +
-	hex.EncodeToString([]byte(newReceipt.Items[0].ShortDescription))[0:4] + "-" +
-	hex.EncodeToString([]byte(newReceipt.PurchaseTime))[0:4] + "-" +
-	hex.EncodeToString([]byte(newReceipt.PurchaseDate))[0:4] + "-" +
-	hex.EncodeToString([]byte(newReceipt.Retailer))[0:4]
+	    DescriptionComponent + "-" +
+      	    hex.EncodeToString([]byte(newReceipt.PurchaseTime))[0:4] + "-" +
+	    hex.EncodeToString([]byte(newReceipt.PurchaseDate))[0:4] + "-" +
+	    RetailerComponent
 
 return returnValue
 }
 
 func main() {
+    // Initialize the hashTable
     table = Init(10)
-    fmt.Println("Hello World")
+    // Initialize the gin router
     router := gin.Default()
+    // tie /receipts/{id}/points to the getPoints function
     router.GET("/receipts/:id/*points", getPoints)
+    // tie /receipts/process to the postReceipt function
     router.POST("/receipts/process", postReceipt)
+    // set trusted proxies to nil for safety
     router.SetTrustedProxies(nil)
+    // run the router
     router.Run("localhost:8080")
 }
 
